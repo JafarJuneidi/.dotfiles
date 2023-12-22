d69ba8091f319d849215c419f841fdce56dfd8ee
JafarJuneidi <aljuneidi.jafar@gmail.com>
2023-12-22 19:13:58 +0200

    tmux-cht.sh

--- bin/.local/scripts/tmux-cht.sh
+++ bin/.local/scripts/tmux-cht.sh
@@ -9,7 +9,7 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.config/tmux/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww zsh -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww zsh -c "curl cht.sh/$selected/$query | less -R"
 else
-    tmux neww zsh -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww zsh -c "curl cht.sh/$selected~$query | less -R"
 fi
