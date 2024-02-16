30af0bcbedbdac2404bc268017694fb44d9f2701
JafarJuneidi <aljuneidi.jafar@gmail.com>
2024-02-16 21:32:19 +0200

    can't beat gruvbox

--- nvim/.config/nvim/lua/plugins/color_scheme.lua
+++ nvim/.config/nvim/lua/plugins/color_scheme.lua
@@ -1,37 +1,73 @@
 return {
-    "catppuccin/nvim",
-    config = function()
-        require("catppuccin").setup({
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                harpoon = true,
-                illuminate = true,
-                indent_blankline = {
-                    enabled = false,
-                    scope_color = "sapphire",
-                    colored_indent_levels = false,
-                },
-                mason = true,
-                native_lsp = { enabled = true },
-                notify = true,
-                nvimtree = true,
-                neotree = true,
-                symbols_outline = true,
-                telescope = true,
-                treesitter = true,
-                treesitter_context = true,
-            },
-        })
+	"ellisonleao/gruvbox.nvim",
+	config = function()
+		require("gruvbox").setup({
+			terminal_colors = true, -- add neovim terminal colors
+			undercurl = true,
+			underline = true,
+			bold = true,
+			italic = {
+				strings = true,
+				emphasis = true,
+				comments = true,
+				operators = false,
+				folds = true,
+			},
+			strikethrough = true,
+			invert_selection = false,
+			invert_signs = false,
+			invert_tabline = false,
+			invert_intend_guides = false,
+			inverse = true, -- invert background for search, diffs, statuslines and errors
+			contrast = "", -- can be "hard", "soft" or empty string
+			palette_overrides = {},
+			overrides = {},
+			dim_inactive = false,
+			transparent_mode = false,
+		})
+		vim.cmd.colorscheme("gruvbox")
 
-        vim.cmd.colorscheme("catppuccin-macchiato")
-
-        -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
-        for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
-            vim.api.nvim_set_hl(0, group, {})
-        end
-    end,
+		-- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+		for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+			vim.api.nvim_set_hl(0, group, {})
+		end
+	end,
 }
+
+-- return {
+--     "catppuccin/nvim",
+--     config = function()
+--         require("catppuccin").setup({
+--             integrations = {
+--                 cmp = true,
+--                 gitsigns = true,
+--                 harpoon = true,
+--                 illuminate = true,
+--                 indent_blankline = {
+--                     enabled = false,
+--                     scope_color = "sapphire",
+--                     colored_indent_levels = false,
+--                 },
+--                 mason = true,
+--                 native_lsp = { enabled = true },
+--                 notify = true,
+--                 nvimtree = true,
+--                 neotree = true,
+--                 symbols_outline = true,
+--                 telescope = true,
+--                 treesitter = true,
+--                 treesitter_context = true,
+--             },
+--         })
+--
+--         vim.cmd.colorscheme("catppuccin-macchiato")
+--
+--         -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+--         for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+--             vim.api.nvim_set_hl(0, group, {})
+--         end
+--     end,
+-- }
 
 -- gruvbox
 -- return {
