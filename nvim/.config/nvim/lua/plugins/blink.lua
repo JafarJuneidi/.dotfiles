2dea33a04c012ddcc7eab44e023558816249fff3
JafarJuneidi <aljuneidi.jafar@gmail.com>
2025-01-29 21:51:54 +0200

    obsidian completion support

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,58 +1,82 @@
 return {
-    'saghen/blink.cmp',
-    dependencies = 'rafamadriz/friendly-snippets',
-    version = '*',
+    -- add blink.compat
+    {
+        'saghen/blink.compat',
+        -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+        version = '*',
+        -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+        lazy = true,
+        -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+        opts = {},
+    },
+    {
+        'saghen/blink.cmp',
+        dependencies = 'rafamadriz/friendly-snippets',
+        version = '*',
+        opts_extend = { 'sources.default' },
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        keymap = {
-            preset = 'enter',
-            ['<C-y>'] = { 'select_and_accept' },
-        },
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            keymap = {
+                preset = 'enter',
+                ['<C-y>'] = { 'select_and_accept' },
+            },
 
-        appearance = {
-            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- Useful for when your theme doesn't support blink.cmp
-            -- Will be removed in a future release
-            use_nvim_cmp_as_default = false,
-            -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
-            nerd_font_variant = 'mono',
-        },
+            appearance = {
+                -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+                -- Useful for when your theme doesn't support blink.cmp
+                -- Will be removed in a future release
+                use_nvim_cmp_as_default = false,
+                -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = 'mono',
+            },
 
-        completion = {
-            accept = {
-                -- experimental auto-brackets support
-                auto_brackets = {
+            completion = {
+                accept = {
+                    -- experimental auto-brackets support
+                    auto_brackets = {
+                        enabled = true,
+                    },
+                },
+                menu = {
+                    draw = {
+                        treesitter = { 'lsp' },
+                    },
+                },
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 100,
+                },
+                ghost_text = {
                     enabled = true,
                 },
             },
-            menu = {
-                draw = {
-                    treesitter = { 'lsp' },
-                },
-            },
-            documentation = {
-                auto_show = true,
-                auto_show_delay_ms = 100,
-            },
-            ghost_text = {
-                enabled = true,
-            },
-        },
 
-        sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
-            cmdline = {},
-            providers = {
-                lazydev = {
-                    name = 'LazyDev',
-                    module = 'lazydev.integrations.blink',
-                    score_offset = 100, -- show at a higher priority than lsp
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'obsidian', 'obsidian_new', 'obsidian_tags' },
+                cmdline = {},
+                providers = {
+                    lazydev = {
+                        name = 'LazyDev',
+                        module = 'lazydev.integrations.blink',
+                        score_offset = 100, -- show at a higher priority than lsp
+                    },
+                    obsidian = {
+                        name = 'obsidian',
+                        module = 'blink.compat.source',
+                    },
+                    obsidian_new = {
+                        name = 'obsidian_new',
+                        module = 'blink.compat.source',
+                    },
+                    obsidian_tags = {
+                        name = 'obsidian_tags',
+                        module = 'blink.compat.source',
+                    },
                 },
             },
         },
     },
-    opts_extend = { 'sources.default' },
 }
